171bf0a787a870610f7dc6dbe36aa9c6095b148a
edmund-h <447ght@gmail.com>
2017-10-23 12:22:42 -0400

    initial commit: create project

---
+++ weatherapp/weatherapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherapp
+//
+//  Created by Edmund Holderbaum on 10/23/17.
+//  Copyright © 2017 Bozo Design Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
